dd0c13077a6f5659ade4eaf06c3797f8d5c6294b
Sara Walker <sarawall1187@gmail.com>
2019-02-12 16:26:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,10 @@ attr_reader :id
 
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+
+  def self.create 
+
+  end
 end
 # Remember, you can access your database connection anywhere in this class
 #  with DB[:conn]
