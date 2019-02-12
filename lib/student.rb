174e2d897eaed702464c55a0c8c312da237bb8c3
Sara Walker <sarawall1187@gmail.com>
2019-02-12 16:27:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,10 @@ attr_reader :id
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
 
   end
 
