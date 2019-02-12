78acb51c6d91d3a28dfb31ff751e66324cde1c76
Sara Walker <sarawall1187@gmail.com>
2019-02-12 16:25:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,12 +29,12 @@ attr_reader :id
 
   def save
     sql = <<-SQL
-          INSERT INTO students (name, grade) 
+          INSERT INTO students (name, grade)
           VALUES (?, ?)
         SQL
-     
+
         DB[:conn].execute(sql, self.name, self.grade)
-     
+
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 end
